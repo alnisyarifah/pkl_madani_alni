b106a1ad661976cc6a32e27ca7ccc764de3d9fdb
alnisyarifah <alnisyarifahsyarifah200@gmail.com>
2024-12-27 14:20:12 +0700

    first commit

---
+++ resources/views/admins/addbanner.blade.php
@@ -0,0 +1,33 @@
+<x-layout-component>
+    <!-- Table Start -->
+    <div class="container-fluid pt-4 px-4">
+  <!-- Form Start -->
+  <div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-6">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Form Tambah Banner</h6>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Judul</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1"
+                            aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">deskripsi</label>
+                        <input type="text" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">gambar</label>
+                        <input type="file" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Form End -->
+         </div>
+         <!-- Table End -->
+</x-layout-component>
